282cb6188bbf709ec5ddcd5ba9919e4ddcd2c625
Sanjana Raghuwanshi <raghuneha2803@gmail.com>
2025-11-01 16:55:02 +0530

    Updated secure pwd

--- DevTinder-backend/config/database.js
+++ DevTinder-backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://raghuneha2803:Neha1111@cluster0.zlsqh.mongodb.net/devTinder')
+    await mongoose.connect('mongodb+srv://raghuneha2803:qKDhdSeGcdgWyT2p@cluster0.zlsqh.mongodb.net/devTinder')
 }
 
 module.exports = connectDB;
